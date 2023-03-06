ba16ea7bfb2809f292e4a307bf5c1d1790e571c3
samandarsaburov <saburovsamandar@gmail.com>
2023-03-06 22:12:39 +0500

    login page

--- js/login.js
+++ js/login.js
@@ -1,19 +1,30 @@
- const userInp = document.querySelector("#loginUser")
- const passwordInp = document.querySelector("#loginPassword")
- const loginForm = document.querySelector("#loginForm")
+const userInp = document.querySelector("#loginUser")
+const passwordInp = document.querySelector("#loginPassword")
+const loginForm = document.querySelector("#loginForm")
+const formBtn = document.querySelector("#formBtn")
 
 //  let user = []
 
- loginForm.addEventListener("submit" , async function(e){
-    e.preventDefault()
+formBtn.addEventListener("click", async function () {
+    // e.preventDefault()
 
     // user.push({"email": userInp.value, "password": passwordInp.value})
-    await fetch("https://reqres.in/api/login", {
-        body: {"email": userInp.value, "password": passwordInp.value},
-        headers: {
-            "Content-type": "application/json"
-        },
-        method: "POST",
-    }) 
-    // console.log(user);
- })
+    let postLogin = await fetch("https://reqres.in/api/login  ",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({ "email": userInp.value, "password": passwordInp.value })
+        })
+    if (postLogin.status === 200) {
+        formBtn.setAttribute("href", "./index.html")
+    }
+    console.log(postLogin.status);
+
+    // .catch(error => formBtn.setAttribute("href", "./index.html"))
+    // console.log("success");
+    // formBtn.setAttribute("disabled")
+    // console.log("error");
+})
